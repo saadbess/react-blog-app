9368bf0a9c30a16d0472bec38bbeea03c2e63de3
saad <saad@koubeissi.com>
2019-06-28 22:11:50 +0100

    Setup backend

---
+++ blog-frontend/src/pages/AboutPage.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from "react";
+
+const AboutPage = () => (
+  <Fragment className="about-page">
+    <h1>About</h1>
+
+    <p>This is the about page</p>
+  </Fragment>
+);
+export default AboutPage;
